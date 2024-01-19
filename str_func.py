9fac406fa38137d54f576735d41ed49c4a9d1d9d
FlintJames <124516283+FlintJames@users.noreply.github.com>
2024-01-19 03:15:18 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,11 +1,13 @@
-'Возвращает строку в верхнем регистре'
 
-s = input()
-s_upper = s.upper()
-print(s_upper)
+def upp():
+  '''Возвращает строку в верхнем регистре'''
+  s = input()
+  s_upper = s.upper()
+  print(s_upper)
 
-'Возвращает строку с первой буквой слова в верхнем регистре'
-s_1 = input()
-s_1_title = s_1.title()
-print(s_1_title)
+def tit():
+  '''Возвращает строку с первой буквой слова в верхнем регистре'''
+  s_1 = input()
+  s_1_title = s_1.title()
+  print(s_1_title)
 
